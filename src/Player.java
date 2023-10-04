2719eca6ba2cc323a5ab84f974a62214ac63d6ed
Arfi <arma0001@stud.kea.dk>
2023-10-04 09:59:11 +0200

    lille opdatering

--- src/Player.java
+++ src/Player.java
@@ -2,6 +2,9 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Player {
+private Die die;
+private Game game;
+
 
     private int postion;
     private int soldiers;
@@ -9,40 +12,79 @@ public class Player {
     private int bombs;
 
     public Player(int postion, int soldiers, int firepower, int bombs) {
-        this.postion = postion;
+        this.soldiers = soldiers;
         this.firepower = firepower;
         this.bombs = bombs;
         this.postion = postion;
-    }
-
-
-
-    public int getPostion() {
-        return postion;
+        die = new Die();
+        game = new Game();
     }
 
     public int getSoldiers() {
         return soldiers;
     }
-
+    public void setSoldiers(int soldiers) {
+        this.soldiers = soldiers;
+    }
     public int getFirepower() {
         return firepower;
+    }
+    public void setFirepower(int firepower) {
+        this.firepower = firepower;
     }
 
     public int getBombs() {
         return bombs;
     }
 
+    public void setBombs(int bombs) {
+        this.bombs = bombs;
+    }
+
+    public int getPostion() {
+        return postion;
+    }
+
+    public void setPostion(int postion) {
+        this.postion = postion;
+    }
 
     public void moveForward() {
+        // hvis dice er lig med et heltal, rykker enemy position 2 frem
 
+        if (die.roll() % 2 == 0) {
+            postion += 2;
+            System.out.println("Position: " + postion);
+        } else {
+            postion += 1;
+            System.out.println("Position: " + postion);
+        }
 
 
     }
 
     public void moveBackwards() {
 
+        int dieRollResult = die.roll();
+
+        if (dieRollResult <= 2) {
+            // Træk 1 felt tilbage
+            postion += 1;
+        } else if (dieRollResult == 3 || dieRollResult == 4) {
+            // Træk 2 felter tilbage
+            postion += 2;
+        } else {
+            // Træk 3 felter tilbage
+            postion += 3;
+        }
+        firepower += 250;
+
+        if (postion == 10 && bombs == 0) {
+            bombs = 1;
+        }
     }
+
+
 
     public void attack() {
        /* //terningen slås igen
@@ -63,6 +105,10 @@ public class Player {
     }
 
     public void detonateBomb() {
+
+    }
+
+    public void spejder () {
 
     }
 
